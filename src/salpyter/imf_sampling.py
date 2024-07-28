241ac0ecb34bec8ffe027e2bb9829715202a1107
Mike Grudic <mike.grudich@gmail.com>
2024-07-27 20:18:56 -0700

    refactor: namespace tweaks

--- src/salpyter/imf_sampling.py
+++ src/salpyter/imf_sampling.py
@@ -1,10 +1,23 @@
 """Routine for generating samples from a certain IMF"""
 
+from .imfs import *
+
+
 def imf_samples(num_samples, imf, params, logmmin=-3, logmmax=2):
     """Uses rejection sampling to sample from an IMF"""
+
+    if isinstance(imf, str):
+        imf_function = get_imf_function(imf)
+    else:
+        imf_function = imf
+
     x = np.random.rand(num_samples)
     logm = logmmin + (logmmax - logmmin) * x
-    imf_val = imf(logm, params)
+    imf_val = imf_function(logm, params)
     imf_max = imf_val.max()
     y = imf_max * np.random.rand(num_samples)
     return 10 ** logm[y < imf_val]
+
+
+def get_imf_function(imf_name: str):
+    return locals()["imf_name"]()
